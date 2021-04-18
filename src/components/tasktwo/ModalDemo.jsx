65478aa1e9d39878d35b7ec453faf8f91072b0b6
Ivan Iaroslavtsev <ivya@ciklum.com>
2021-04-18 23:57:52 +0300

    Task Two: create a demo page and include it to the TaskTwo component

---
+++ src/components/tasktwo/ModalDemo.jsx
@@ -0,0 +1,151 @@
+import React from 'react';
+import styled from 'styled-components';
+import Modal from '../modal';
+import { ColorType, Size } from '../ui/theme';
+import ButtonDefault from '../ui/Button';
+import Spinner from '../ui/Spinner';
+import { FlexColumn, FlexLine } from '../ui/Flex';
+import Text from '../ui/Text';
+import useModals from '../modal/useModals';
+import DeleteHistory from '../report/DeleteHistory';
+
+const Button = styled(ButtonDefault)`
+  margin-right: 5px;
+`;
+
+const Modals = {
+  spinner: 'spinner',
+  confirmation: 'confirmation',
+  deletion: 'deletion',
+};
+
+const ModalDemo = () => {
+  const {
+    showModal, closeModal, isShown, closeAll, anyShown
+  } = useModals();
+
+  return (
+    <>
+      <FlexLine>
+        <Button
+          onClick={() => showModal(Modals.spinner, true)}
+          colorType={ColorType.primary}
+        >
+          Show spinner modal
+        </Button>
+        <Button
+          onClick={() => {
+            showModal(Modals.confirmation, true);
+          }}
+          colorType={ColorType.secondary}
+        >
+          Show confirmation modal
+        </Button>
+        <Button
+          onClick={() => showModal(Modals.deletion, true)}
+          colorType={ColorType.danger}
+        >
+          Show deletion modal
+        </Button>
+        {anyShown() && (
+          <Button onClick={closeAll} fadeIn>
+            Close All
+          </Button>
+        )}
+      </FlexLine>
+
+      {isShown(Modals.spinner) && (
+        <Modal
+          priority={2}
+          color={ColorType.danger}
+          styles={{
+            ModalWrapper: {
+              height: '150px',
+              width: '400px',
+              justifyContent: 'center',
+              alignItems: 'center',
+            },
+          }}
+        >
+          <FlexColumn>
+            <Spinner size={Size.x2} />
+            <Text size={Size.x05} colorType={ColorType.secondary}>
+              Data is loading
+            </Text>
+          </FlexColumn>
+        </Modal>
+      )}
+
+      {isShown(Modals.confirmation) && (
+        <Modal
+          headingText="Are you sure you want to delete all of your files?"
+          color={ColorType.danger}
+          styles={{
+            Button: {
+              width: '100px',
+              marginLeft: '10px',
+            },
+            ModalFooter: {
+              justifyContent: 'flex-end',
+            },
+          }}
+          buttons={[
+            {
+              id: 'yes',
+              text: 'Yes',
+              colorType: ColorType.primary,
+              onClick: () => closeModal(Modals.confirmation),
+            },
+            {
+              id: 'no',
+              text: 'No',
+              colorType: ColorType.secondary,
+              onClick: () => closeModal(Modals.confirmation),
+              closeOnClick: true,
+            },
+          ]}
+        >
+          <p>This action can not be undone.</p>
+        </Modal>
+      )}
+
+      {isShown(Modals.deletion) && (
+        <Modal
+          headingText="Are you sure you want to delete this report and its history?"
+          color={ColorType.danger}
+          styles={{
+            ModalWrapper: {
+              width: '500px',
+            },
+            Button: {
+              width: '100px',
+              marginLeft: '10px',
+            },
+            ModalFooter: {
+              justifyContent: 'flex-end',
+            },
+          }}
+          buttons={[
+            {
+              id: 'deleteAll',
+              text: 'Delete all',
+              colorType: ColorType.primary,
+              onClick: () => closeModal(Modals.deletion),
+            },
+            {
+              id: 'cancel',
+              text: 'Cancel',
+              colorType: ColorType.secondary,
+              onClick: () => closeModal(Modals.deletion),
+              closeOnClick: true,
+            },
+          ]}
+        >
+          <DeleteHistory />
+        </Modal>
+      )}
+    </>
+  );
+};
+
+export default ModalDemo;
--- src/components/tasktwo/TaskTwo.jsx
+++ src/components/tasktwo/TaskTwo.jsx
@@ -4,24 +4,25 @@ import {
   DesignList,
   Heading,
   StyledList,
-  Task
+  Task,
 } from '@components/styled-components/Task';
+import ModalDemo from './ModalDemo';
 
 const modals = [
   {
     title: 'Data loading modal: ',
     alt: 'Data loading modal',
-    src: 'images/data_loading.png'
+    src: 'images/data_loading.png',
   },
   {
     title: 'Delete files modal: ',
     alt: 'Delete files modal',
-    src: 'images/delete_files.png'
+    src: 'images/delete_files.png',
   },
   {
     title: 'Delete report and history modal: ',
     alt: 'Delete report and history modal',
-    src: 'images/delete_report_and_history.png'
+    src: 'images/delete_report_and_history.png',
   },
 ];
 
@@ -29,16 +30,19 @@ const TaskTwo = () => (
   <Task>
     <Heading>Task Two</Heading>
     <Content>
+      <h4>The demo page: </h4>
+      <p>Use the buttons to open appropriate modals.</p>
+      <ModalDemo />
+      <hr />
       <h4>Complete the following task:</h4>
       <p>
         This is to assess your ability to design and build React components.
-        Assume that you are part of a team starting a new project,
-        a consistent design language has been set by the Design Team,
-        i.e. fonts, labels, button and input styles etc. Below are
-        designs for some components for you to build. With unit tests
-        and without 3rd party modal packages, build the modals shown in
-        these designs and have re-usability in mind as what you build
-        might be re-usable in other parts of the application.
+        Assume that you are part of a team starting a new project, a consistent
+        design language has been set by the Design Team, i.e. fonts, labels,
+        button and input styles etc. Below are designs for some components for
+        you to build. With unit tests and without 3rd party modal packages,
+        build the modals shown in these designs and have re-usability in mind as
+        what you build might be re-usable in other parts of the application.
       </p>
       <p>
         {`Include unit tests. React Testing Library is already 
@@ -50,28 +54,26 @@ const TaskTwo = () => (
           <strong>No </strong>
           backdrop/page overlay is required for this exercise.
         </li>
-        <li>All modals should expose a mechanism to allow them to be closed.</li>
-        <li>There should be a way to consume button click events on modals with buttons.</li>
-        <li>Create a demo page with three buttons that trigger each of the modals to show.</li>
+        <li>
+          All modals should expose a mechanism to allow them to be closed.
+        </li>
+        <li>
+          There should be a way to consume button click events on modals with
+          buttons.
+        </li>
+        <li>
+          Create a demo page with three buttons that trigger each of the modals
+          to show.
+        </li>
       </StyledList>
       <DesignList>
         {modals.map(({ title, alt, src }, index) => (
           <div key={index.toString()}>
-            <label htmlFor={`img-${index}`}>
-              {title}
-            </label>
-            <img
-              alt={alt}
-              src={src}
-              id={`img-${index}`}
-            />
+            <label htmlFor={`img-${index}`}>{title}</label>
+            <img alt={alt} src={src} id={`img-${index}`} />
           </div>
         ))}
       </DesignList>
-      <hr />
-      <strong>
-        Feel free to use this component as a demo page for your work.
-      </strong>
     </Content>
   </Task>
 );
