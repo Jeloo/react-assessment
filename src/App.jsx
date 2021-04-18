8d44d4f2300accf65bdaed6c213ef96086aa1e07
Ivan Iaroslavtsev <ivya@ciklum.com>
2021-04-18 23:56:05 +0300

    Task Two: apply GlobalStyles to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GlobalStyle from '@components/ui/GlobalStyle';
 import Nav from '@components/Nav';
 import TaskOne from '@components/taskone/TaskOne';
 import TaskTwo from '@components/tasktwo/TaskTwo';
 
 const App = () => (
-  <Router>
-    <Nav />
-    <Switch>
-      <Route path="/" exact component={TaskOne} />
-      <Route path="/task-two" component={TaskTwo} />
-    </Switch>
-  </Router>
+  <>
+    <GlobalStyle />
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={TaskOne} />
+        <Route path="/task-two" component={TaskTwo} />
+      </Switch>
+    </Router>
+  </>
 );
 
 export default App;
